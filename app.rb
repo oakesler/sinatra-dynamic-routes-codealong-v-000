9fecf933c43c0cb8b9923e4f6d7d72f42245034f
Olivia Kesler <oakesler@protonmail.ch>
2019-12-26 21:47:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,6 +19,6 @@ class App < Sinatra::Base
   get "/multiply/:number1/:number2" do
     @num1 = params[:number1]
     @num2 = params[:number2]
-    "#{@num1 + @num2}"
+    "#{@num1 * @num2}"
   end
 end
