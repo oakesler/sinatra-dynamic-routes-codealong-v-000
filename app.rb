e2ab650f072326a46bbab4d258d7848d0080f9f7
Olivia Kesler <oakesler@protonmail.ch>
2019-12-26 20:43:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,18 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  # This is a sample static route.
   get '/hello' do
     "Hello World!"
   end
-
-  # This is a sample dynamic route.
+  
   get "/hello/:name" do
     @user_name = params[:name]
     "Hello #{@user_name}!"
   end
-end
-
   # Code your final two routes here:
   get 'goodbye/:name' do
     @user_name = params[:name]
